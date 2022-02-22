72d8749a67ae7700f1dd1994c44e8e5c609d2b2e
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2022-02-22 15:51:27 +0530

    first commit project initialization

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<StreamList />} />
+      <Route path="/streams/new" element={<StreamCreate />} />
+      <Route path="/streams/edit" element={<StreamEdit />} />
+      <Route path="/streams/delete" element={<StreamDelete />} />
+      <Route path="/streams/show" element={<StreamShow />} />
+    </Routes>
+  );
+};
+
+export default App;
